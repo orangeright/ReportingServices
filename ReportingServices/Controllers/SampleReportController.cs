aa367fe0e5673c3d76a8361abae8bfeda9f55d6f
taguchi <tagutagu@qb3.so-net.ne.jp>
2018-03-19 19:30:29 +0900

    もう少し

--- ReportingServices/Controllers/SampleReportController.cs
+++ ReportingServices/Controllers/SampleReportController.cs
@@ -3,7 +3,9 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,23 +17,34 @@ namespace ReportingServices.Controllers
 {
     public class SampleReportController : Controller
     {
-        private string baseUri = "https://39.110.246.5/pleasanter/api_items/33/get";
-        //private string baseUri = "https://iis-pleasanter.azurewebsites.net/api_items/79/get";
+        //private string baseUri = "http://39.110.246.5/pleasanter/api_items/33/get";
+        //private Hashtable param = new Hashtable
+        //{
+        //    ["ApiKey"] = "09ca5b781f694e4f7fae9299d9a62998cc1bcefa262e11be8299496491a96e4be857c0e838fa5365ee30c8266b96f2b1b1982a3aff88b9dbd2ea1c088f64766e"
+        //};
+
+        private string baseUri = "http://localhost/pleasanter/api_items/98/get";
+        private Hashtable param = new Hashtable
+        {
+            ["ApiKey"] = "40a398c2c4ab22bda378b3650f6cb7ea72eeafa789dd9ef7c4dcbc3f056052db8e0d08c49d0efd3f5d028da5f5b7f36788d5836bc50783f5b1a1844124434496"
+        };
+
 
         // GET: SampleReport
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            var result = GetByIDforAPI();
-
+            ViewBag.Result = await GetByIDforAPI();
+            //ViewBag.Result = GetByIDforAPI2();
             return View();
         }
 
         [HttpPost]
         public async Task<ActionResult> Print()
         {
-
             string result = await GetByIDforAPI();
-            //string j = JsonConvert.
+            Debug.WriteLine("No3");
+            Debug.WriteLine(result);
+
             return View();
 
         }
@@ -42,13 +55,9 @@ namespace ReportingServices.Controllers
             using (HttpClient httpClient = new HttpClient())
             {
 
-                var param = new Hashtable();
-                param["apikey"] = "2ff2c1afbe9667021a1fc828ac6307643526cea32ad1ecef28954b4ce61f3a6b194a40d55831a331bb11cc07eaf6d59c071774245bbcbdb464aa91ad8f5754a5";                           // 数値型のパラメータ
-                //param["apikey"] = "495df7ed6df3cd77aeea7857cf4e77c39ccd6e6b5460b070c902a4b862f73380449143553ac53cad064886727d0b4f4a21830b0290f2847406937ec360f4a4be";                           // 数値型のパラメータ
-                var serializer = new JavaScriptSerializer();
+                //var json = new JavaScriptSerializer().Serialize(param);
 
-                //var json = "{ \"apikey\" : \"2ff2c1afbe9667021a1fc828ac6307643526cea32ad1ecef28954b4ce61f3a6b194a40d55831a331bb11cc07eaf6d59c071774245bbcbdb464aa91ad8f5754a5\" }";
-                var json = serializer.Serialize(param);
+                var json = "{ \"ApiKey\" : \"40a398c2c4ab22bda378b3650f6cb7ea72eeafa789dd9ef7c4dcbc3f056052db8e0d08c49d0efd3f5d028da5f5b7f36788d5836bc50783f5b1a1844124434496\"  }";
 
                 using (var client = new HttpClient())
                 {
@@ -56,12 +65,42 @@ namespace ReportingServices.Controllers
                     var content = new StringContent(json, Encoding.UTF8);
 
                     var response =  await client.PostAsync(uri, content);
+                    Debug.WriteLine("No1");
                     Debug.WriteLine(response);
                     var result = await response.Content.ReadAsStringAsync();
+                    Debug.WriteLine("No2");
                     Debug.WriteLine(result);
                     return result;
                 }
             }
         }
+
+        private string GetByIDforAPI2()
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(baseUri);
+            req.ContentType = "application/json";
+            req.Method = "POST";
+
+            using (var streamwriter = new StreamWriter(req.GetRequestStream()))
+            {
+                string jsonPayload = new JavaScriptSerializer().Serialize(param);
+                streamwriter.Write(jsonPayload);
+            }
+
+            HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+
+            using (res)
+            {
+                using (var resStream = res.GetResponseStream())
+                {
+                    StreamReader sr = new StreamReader(resStream, Encoding.UTF8);
+                    string result = sr.ReadToEnd();
+                    Debug.WriteLine("No11");
+                    Debug.WriteLine(result);
+                    return result;
+                }
+            }
+        }
+
     }
 }
